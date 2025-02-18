459fc1b5e5bf9dd7d8272d06ed19a01e8ea185bc
TeerdhaTeerdha <120081903+TeerdhaTeerdha@users.noreply.github.com>
2025-02-18 18:02:29 +0530

    Add files via upload

---
+++ 1.d arithmetic operator (convert the dollar to rs and rs to dollar).py
@@ -0,0 +1,9 @@
+# write a program to convert Dollar to RS
+dollar=int(input("Enter the dolalr:"))
+rs=dollar*80
+print(dollar,rs)
+
+# write a program to convert RS to Dollar
+rs=int(input("Enter Rs:"))
+dollar=rs/80
+print(rs,int(dollar))
