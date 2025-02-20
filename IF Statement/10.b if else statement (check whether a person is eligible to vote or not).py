2383c01fe04c36953973a55f2e30bbc5ae884162
TeerdhaTeerdha <120081903+TeerdhaTeerdha@users.noreply.github.com>
2025-02-20 11:53:25 +0530

    Add files via upload

---
+++ IF Statement/10.b if else statement (check whether a person is eligible to vote or not).py
@@ -0,0 +1,7 @@
+# write a program to check whether a person is eligible for vote or not
+age=int(input("Enter the age: "))
+if age >=18:
+    print("person is eligible to vote")
+else:
+    print("person is not eligible to vote")
+    
