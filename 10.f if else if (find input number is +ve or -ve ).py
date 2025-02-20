a0ea696f69ad33326694d745d9a6448f1156f0a3
TeerdhaTeerdha <120081903+TeerdhaTeerdha@users.noreply.github.com>
2025-02-20 11:55:41 +0530

    Delete 10.f if else if (find input number is +ve or -ve ).py

--- 10.f if else if (find input number is +ve or -ve ).py
+++
@@ -1,8 +0,0 @@
-# write a orogram to find input number is (+ve) or (-ve) or zero
-num=int(input("Enter any number"))
-if num > 0:
-    print(f'{num} is a positive number')
-elif num < 0:
-    print(f'{num} is a negative number')
-else:
-    print(f'{num} is zero')
