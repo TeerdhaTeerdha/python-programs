9805b913af32a8f9b308e21100eea015f24255c5
TeerdhaTeerdha <120081903+TeerdhaTeerdha@users.noreply.github.com>
2025-02-20 11:54:34 +0530

    Delete 10.b if else statement (check whether a person is eligible to vote or not).py

--- 10.b if else statement (check whether a person is eligible to vote or not).py
+++
@@ -1,7 +0,0 @@
-# write a program to check whether a person is eligible for vote or not
-age=int(input("Enter the age: "))
-if age >=18:
-    print("person is eligible to vote")
-else:
-    print("person is not eligible to vote")
-    
